6ef030521fe9c29d7f96d6d229894069dd5fadb6
https://github.com/nurseincode <nurseincode@gmail.com>
2025-09-08 23:47:12 +1000

    Data types

---
+++ main.py
@@ -0,0 +1,26 @@
+# print(2 + 3) # My first function comment
+# print('Hello Python!')
+
+# Data Types
+
+# Numbers
+# Int
+# 5
+
+# Finding the data type by calling the type function and passing the value. Output: <class 'int'>
+print(type(5))
+
+# Float
+# 3.14159 pi approx
+
+print(type(3.14159))
+
+# Complex Numbers
+print(type(2 + 3j))
+
+# Number Operations
+print(2 + 3)
+print(2 - 3)
+print(2 * 3) # Multiplication asteric symbol
+print(10 / 5) # / division operator always returns a float, even if the result is a whole number. Output: 2.0
+print(10 // 5) # Use the floor division operator: // if you want an integer result. Output: 2
